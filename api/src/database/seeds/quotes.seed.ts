1f0784505c82d28bdae364c67cd473eb6f5d5239
Micaelli Medeiros <m.amedeiros@hotmail.com>
2023-10-19 13:31:47 -0300

    chore: add more seeds

--- api/src/database/seeds/quotes.seed.ts
+++ api/src/database/seeds/quotes.seed.ts
@@ -6,12 +6,12 @@ exports.seed = function (knex: Knex) {
     .then(function () {
       return knex("quotes").insert([
         {
-          departure_location: "New York Airport",
-          destination_location: "Los Angeles Airport",
+          departure_location: "São Paulo Airport",
+          destination_location: "Toronto Airport",
           departure_date: "2023-10-15",
           return_date: "2023-10-22",
           number_of_travelers: 2,
-          transportation: "Rental Car",
+          transportation: "Airport",
           contact_information: "john.doe@example.com",
         },
         {
@@ -22,6 +22,33 @@ exports.seed = function (knex: Knex) {
           number_of_travelers: 4,
           transportation: "Flight",
           contact_information: "jane.smith@example.com",
+        },
+        {
+          departure_location: "New York Airport",
+          destination_location: "Los Angeles Airport",
+          departure_date: "2023-10-15",
+          return_date: "2023-10-22",
+          number_of_travelers: 2,
+          transportation: "Rental Car",
+          contact_information: "ph.enders@gmail.com",
+        },
+        {
+          departure_location: "Campinas City",
+          destination_location: "São Paulo City",
+          departure_date: "2023-11-05",
+          return_date: "2023-11-12",
+          number_of_travelers: 4,
+          transportation: "Rental Car",
+          contact_information: "amaz@example.com",
+        },
+        {
+          departure_location: "São Paulo Airport",
+          destination_location: "Canada Airport",
+          departure_date: "2023-10-15",
+          return_date: "2023-10-22",
+          number_of_travelers: 2,
+          transportation: "Airport",
+          contact_information: "jack@example.com",
         },
       ]);
     });
